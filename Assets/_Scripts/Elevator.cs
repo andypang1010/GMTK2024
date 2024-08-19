c1d09715af1872451dd58b3967f28bf5af0065f4
Andy Pang <andypang1010@gmail.com>
2024-08-18 23:33:04 -0400

    Lobby elevator

--- Assets/_Scripts/Elevator.cs
+++ Assets/_Scripts/Elevator.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Elevator : MonoBehaviour
 {
+    public bool isActive;
+    public MoveType moveType;
     public float moveSpeed;
     public Transform[] points;
     public int startIndex;
@@ -18,17 +22,38 @@ public class Elevator : MonoBehaviour
     }
 
     private void Update() {
-        if (Vector2.Distance(transform.position, points[index].position) < 0.02f) {
-            index++;
+        if (!isActive) return;
 
-            index %= points.Length;
+        if (moveType == MoveType.OSCILLATE) {
+            if (Vector2.Distance(transform.position, points[index].position) < 0.02f) {
+                index++;
+
+                index %= points.Length;
+            }
+
+            transform.position = 
+                Vector2.MoveTowards(transform.position, points[index].position, moveSpeed * Time.deltaTime);
         }
 
-        transform.position = 
-            Vector2.MoveTowards(transform.position, points[index].position, moveSpeed * Time.deltaTime);
+        else if (moveType == MoveType.SINGLE) {
+            if (Vector2.Distance(transform.position, points[0].position) > 0.02f)
+            transform.position = 
+                Vector2.MoveTowards(transform.position, points[0].position, moveSpeed * Time.deltaTime);
+
+        }
+
     }
 
     public void ResetElevator() {
         transform.position = startPosition;
+    }
+
+    public void ActivateElevator() {
+        isActive = true;
+    }
+
+    public enum MoveType {
+        OSCILLATE,
+        SINGLE
     }
 }
